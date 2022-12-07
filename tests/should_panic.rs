11ccd2b36ad42d26ade396f4cb932be6172b6ec6
Greg <gjmooney@asu.edu>
2022-12-07 15:12:47 +0100

    Disable harness

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,28 +1,16 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use rust_os::{QemuExitCode, exit_qemu, serial_println, serial_print};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
-}
-
-// Redefine test_runner to succeed on panics
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
 }
 
 #[panic_handler]
@@ -32,7 +20,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
 fn should_fail() {
     // manually print name since there's no Testable trait here
     serial_print!("should_panic::should_fail...\t");
