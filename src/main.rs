dec8932487858bc31d4a059e4a428a6c44af9023
Greg <gjmooney@asu.edu>
2022-12-07 14:34:52 +0100

    Implement testing and print to console

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![no_std] // don't link the standard library
 #![no_main] // disable rust level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
-
+mod serial;
 
 // Overwrite OS entry point
 // disables name mangling as we need to know
@@ -15,14 +18,61 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     println!("Hell World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 // Called on panic
 // ! is 'never' type, does not return
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(0, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
