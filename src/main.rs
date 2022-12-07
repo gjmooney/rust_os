3b64f44305ed76e0c0cb796fa96834e7a64e8538
Greg <gjmooney@asu.edu>
2022-12-07 15:10:28 +0100

    Integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 #![no_std] // don't link the standard library
 #![no_main] // disable rust level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-mod serial;
+use rust_os::println;
 
 // Overwrite OS entry point
 // disables name mangling as we need to know
 // the exact name to pass to the linker
 // extern C tells compiler to use C calling convention
-// ! is divergin funtion, does not return
+// ! is diverging function, does not return
 #[no_mangle] 
 pub extern "C" fn _start() -> ! {
     println!("Hell World{}", "!");
@@ -37,56 +35,10 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
+    rust_os::test_panic_handler(info);
 }
 
 #[test_case]
 fn trivial_assertion() {
     assert_eq!(1, 1);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T 
-where 
-    T: Fn(),
-{
-    fn run(&self) {
-        // print test function name
-        serial_println!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
 }
