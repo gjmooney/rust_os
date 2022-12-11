5af48e32e9e2e936d0408fd73c9a51d724c411b4
Greg <gjmooney@asu.edu>
2022-12-11 15:59:40 +0100

    Implement double fault handling

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,12 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
+    fn stack_overflow() {
+        stack_overflow();
+    }
+
+    stack_overflow();
+
     // Invoke breakpoint interrupt
     x86_64::instructions::interrupts::int3();
 
