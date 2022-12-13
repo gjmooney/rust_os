a351e65cdf19fec98592be687fdebbb95806bc27
Greg <gjmooney@asu.edu>
2022-12-13 13:27:21 +0100

    View contents of page tables

--- src/main.rs
+++ src/main.rs
@@ -17,31 +17,40 @@ entry_point!(kernel_main);
 // extern C tells compiler to use C calling convention
 // ! is diverging function, does not return
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("Hell World{}", "!");
+    use rust_os::memory::active_level_4_table;
+    use x86_64::VirtAddr;
 
+    println!("Hell World{}", "!");
     rust_os::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!(
-        "Level 4 page table at: {:?}",
-        level_4_page_table.start_address()
-    );
-    fn stack_overflow() {
-        stack_overflow();
+    for (i, entry) in l4_table.iter().enumerate() {
+        use x86_64::structures::paging::PageTable;
+
+        if !entry.is_unused() {
+            println!("L4 Entry {}: {:?}", i, entry);
+
+            // Get physical address from entry and convert it
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            // Print entries of l3 table
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!(" L3 Entry {}: {:?}", i, entry);
+                }
+            }
+        }
     }
-
-    //stack_overflow();
-
-    // Invoke breakpoint interrupt
-    //x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
 
     println!("It didn't crash!");
-
     rust_os::hlt_loop();
 }
 
