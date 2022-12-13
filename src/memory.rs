94cdbfda6a7fc97f5f0d4dacb2fb868619b5bb56
Greg <gjmooney@asu.edu>
2022-12-13 13:48:44 +0100

    Map virtual addr to phys addr

--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,4 @@
-use x86_64::{structures::paging::PageTable, VirtAddr};
+use x86_64::structures::paging::{OffsetPageTable, PageTable, PhysAddr, VirtAddr};
 
 // Get a mutable reference to the active level 4 table
 // Unsafe because caller must guarantee complete physical memory is mapped
@@ -15,3 +15,47 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
 
     &mut *page_table_ptr
 }
+
+// Translate virtual address to physical address (or None)
+// Unsafe because call must guarantee physical memory is mapped
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+// Private function called by translate_addr to limit unsafe calls
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::registers::control::Cr3;
+    use x86_64::structures::paging::page_table::FrameError;
+
+    // get active level 4 frame
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p4_index(),
+        addr.p3_index(),
+        addr.p2_index(),
+        addr.p1_index(),
+    ];
+    let mut frame = level_4_table_frame;
+
+    // traverse the page table levels
+    for &index in &table_indexes {
+        // convert frame into page table reference
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe { &*table_ptr };
+
+        // read entry and update frame
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("Huge pages not supported"),
+        };
+    }
+
+    // calculate physical address by adding offset
+    Some(frame.start_address() + u64::from(addr.page_offset()))
+}
+
+// initialize new offset page table
